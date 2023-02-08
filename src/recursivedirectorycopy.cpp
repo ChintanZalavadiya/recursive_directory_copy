b92bc2cf89c5a5ddef9592349e6ca9744465c3fe
ChintanZalavadiya <124841735+ChintanZalavadiya@users.noreply.github.com>
2023-02-08 23:43:32 +0000

    Update recursivedirectorycopy.cpp

--- src/recursivedirectorycopy.cpp
+++ src/recursivedirectorycopy.cpp
@@ -10,16 +10,16 @@ int main(int argc, char **argv)
       
     if ( argc >= 3)
     {
-		try
-		{     
-			   // argv[1] is srcdirpath and argv[2] is desdirpath	
-			fs:: copy(argv[1], argv[2] , fs::copy_options::overwrite_existing|fs::copy_options::recursive);
-			LOG(LOG_INFO,"copy successful");
-		}
-		catch(exception& e)
-		{
-			LOG(LOG_ERROR,e.what());
-		}
+	try
+	{     
+	       // argv[1] is srcdirpath and argv[2] is desdirpath	
+	       fs:: copy(argv[1], argv[2] , fs::copy_options::overwrite_existing|fs::copy_options::recursive);
+	       LOG(LOG_INFO,"copy successful");
+	}
+	catch(exception& e)
+	{
+	       LOG(LOG_ERROR,e.what());
+	}
     }
     return 0;
 }
